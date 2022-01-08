9ae28618dbf9edc67ff82cb9cf379847b6cb6a22
gitmemet@vsmrk.ru <gitmemet@vsmrk.ru>
2022-01-08 10:49:07 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Context } from 'context';
+import { Context } from 'context.js';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
